8be325efc5157c691ebbffe09a04b146328c0d04
Kirill_Lavrov <kirill.lavrov@checloud.ru>
2026-06-10 23:56:09 +0300

    modified:   xray-generate-config.py

--- xray-generate-config.py
+++ xray-generate-config.py
@@ -324,9 +324,9 @@ def base_config() -> dict:
                 "tag": "tproxy-in",
                 "listen": "0.0.0.0",
                 "port": 12345,
-                "protocol": "tunnel",
+                "protocol": "dokodemo-door",
                 "settings": {
-                    "allowedNetwork": "tcp,udp",
+                    "network": "tcp,udp",
                     "followRedirect": True
                 },
                 "streamSettings": {
@@ -344,9 +344,9 @@ def base_config() -> dict:
                 "tag": "dns-in",
                 "listen": "127.0.0.1",
                 "port": 5353,
-                "protocol": "tunnel",
+                "protocol": "dokodemo-door",
                 "settings": {
-                    "allowedNetwork": "tcp,udp"
+                    "network": "tcp,udp"
                 }
             }
         ]
